7c50ea66c026e51e53ba284191be6b4415db6ebb
Jon Wood <jw@jmwmbp.T-mobile.com>
2018-07-28 04:44:51 -0700

    watch button text

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to design and code React apps</h1>
+    <p>
+      Complete courses about hte best tools and design systems. Protoype and
+      build apps with React and Swift.
+    </p>
+    <Link to="/page-2/">Watch the video</Link>
+  </div>
+)
+
+export default IndexPage
