302383c539553b52131fe95fbbb2561b5509b485
Jon Wood <jw@jmwmbp.T-mobile.com>
2018-07-28 06:13:42 -0700

    added basic css as well as media queries & grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to design and code React apps</h1>
-    <p>
-      Complete courses about hte best tools and design systems. Protoype and
-      build apps with React and Swift.
-    </p>
-    <Link to="/page-2/">Watch the video</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>Complete courses about hte best tools and design systems. Protoype and
+          build apps with React and Swift.</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
   </div>
 )
 
