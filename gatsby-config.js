50bd349df8b862f2947dfe7fdfdfbdb696e66fbe
Jon Wood <jw@jmwmbp.T-mobile.com>
2018-07-31 16:18:05 -0700

    Injected data from Contentful CMS for the Footer section. queryed graphQL and applied styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: 'Design+Code 3',
+    description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
+    keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'ed7f53nni61l',
+        accessToken: '1b8603594d95cb50597e506dfe898ce3cafc88fc66cbdab2cfac25642ace1538'
+      }
+    }
+  ],
 }
